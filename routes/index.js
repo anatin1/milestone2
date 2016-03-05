ca5b20de6bfc50bd85452c398c0606c1da2e5cb4
Austin Coelho <anatin9@yahoo.com>
2016-03-05 08:07:25 -0700

    -attempted to make working array, failed.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+var entries = [
+{slug: "how to pass class", body: "come to class"},
+{slug: "test", body: "second"}
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
--- routes/til.js
+++ routes/til.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var entries = [
+{slug: "how to pass class", body: "come to class"},
+{slug: "test", body: "second"}
+];
 /* GET til listing. */
 router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Today I Learned' });
+  res.render('til/index', { title: 'Today I Learned'});
 });
 
 module.exports = router;
